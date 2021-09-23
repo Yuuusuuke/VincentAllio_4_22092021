3b23eea02093f2d9bbae6bccc1da64b0cbe91f9e
Yuusuke <masngelox7@gmail.com>
2021-09-23 22:55:47 +0200

    Implement form entries and validate them

--- js/modal.js
+++ js/modal.js
@@ -21,8 +21,168 @@ function launchModal() {
 }
 
 // close modal form
-function closeModal(){
+function closeModal() {
   modalbg.style.display = "none";
 }
 
+/* - - - Form - - - */
 
+// Inputs
+const firstForm = document.getElementById("first");
+const lastForm = document.getElementById("last");
+const emailForm = document.getElementById("email");
+const birthdateForm = document.getElementById("birthdate");
+const quantityForm = document.getElementById("quantity");
+const locationForm = document.querySelectorAll(".checkbox-input[type=radio]");
+const conditionForm = document.getElementById("checkbox1");
+const condiCheckbox = document.getElementsByClassName("check")[0];
+
+// colors
+const colorDefault = "#279e7a";
+const colorGrey = "#ccc";
+
+// Show Error
+function displayError(input) {
+  input.parentNode.getElementsByClassName("errorValidation")[0].style.display = "block";
+}
+
+// Hide Error
+function hideError(input) {
+  input.parentNode.getElementsByClassName("errorValidation")[0].style.display = "none";
+}
+
+// Change the border color of the input
+function border(input, color) {
+  input.style.borderColor = color;
+}
+
+// Change the border color of all radio boxes
+function borderRadio(color) {
+  var radios = document.querySelectorAll(".radio");
+  radios.forEach(child => {
+    child.style.borderColor = color;
+  });
+}
+
+// Hide all errors and remove red border color
+function resetErrors() {
+  hideError(firstForm);
+  border(firstForm, colorGrey);
+
+  hideError(lastForm);
+  border(lastForm, colorGrey);
+
+  hideError(emailForm);
+  border(emailForm, colorGrey);
+
+  hideError(birthdateForm);
+  border(birthdateForm, colorGrey);
+
+  hideError(quantityForm);
+  border(quantityForm, colorGrey);
+
+  hideError(locationForm[1]);
+  borderRadio(colorDefault);
+
+  hideError(conditionForm);
+  border(condiCheckbox, colorDefault);
+}
+
+function checkFirst() {
+  return (firstForm.value !== null && firstForm.value.length >= 2)
+}
+
+function checkLast() {
+  return (lastForm.value !== null && lastForm.value.length >= 2)
+}
+
+function checkEmail() {
+  return /^\S+@\S+\.\S+$/.test(emailForm.value);
+}
+
+function checkBirthdate() {
+  let birthdate = new Date(birthdateForm.value);
+  let today = new Date();
+
+  if (birthdate.toString() !== "Invalid Date") {
+
+    return !( // Check if the date isn't after today
+      birthdate.getFullYear() > today.getFullYear() ||
+      (birthdate.getFullYear() == today.getFullYear() && birthdate.getMonth() > today.getMonth()) ||
+      (birthdate.getFullYear() == today.getFullYear() && birthdate.getMonth() == today.getMonth() && birthdate.getDate() > today.getDate())
+    );
+
+  } else {
+    return false;
+  }
+}
+
+function checkQuantity() {
+  return /^[0-9]+$/.test(quantityForm.value);
+}
+
+function checkLocation() {
+  for (let radio of locationForm) {
+    if (radio.checked === true) return true;
+  }
+  return false;
+}
+
+function checkCondition() {
+  return conditionForm.checked;
+}
+
+function validate(event) {
+  event.preventDefault();
+
+  var valide = true;
+  resetErrors();
+
+
+  if (!checkFirst()) {
+    displayError(firstForm);
+    border(firstForm, "red");
+    valide = false;
+  }
+
+  if (!checkLast()) {
+    displayError(lastForm);
+    border(lastForm, "red");
+    valide = false;
+  }
+
+  if (!checkEmail()) {
+    displayError(emailForm);
+    border(emailForm, "red");
+    valide = false;
+  }
+
+  if (!checkBirthdate()) {
+    displayError(birthdateForm);
+    border(birthdateForm, "red");
+    valide = false;
+  }
+
+  if (!checkQuantity()) {
+    displayError(quantityForm);
+    border(quantityForm, "red");
+    valide = false;
+  }
+
+  if (!checkLocation()) {
+    displayError(locationForm[1]);
+    borderRadio("red");
+    valide = false;
+  }
+
+  if (!checkCondition()) {
+    displayError(conditionForm);
+    border(condiCheckbox, "red");
+    valide = false;
+  }
+
+  if (valide) {
+    alert("Tout est bon !");
+  }
+
+}
